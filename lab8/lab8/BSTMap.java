3f7f3abded0c7e6ab39e7af703ee801f570304c6
Allan Wong <allanwong94@163.com>
2017-05-19 22:00:04 +0800

    Lab 8 completed.

--- lab8/lab8/BSTMap.java
+++ lab8/lab8/BSTMap.java
@@ -3,107 +3,105 @@ package lab8;
 import java.util.Iterator;
 import java.util.Set;
 
-import com.sun.corba.se.impl.orbutil.graph.Node;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
+	private BST T;
 	
-	
-	private K key;
-	private V val;
-	public BSTMap left;
-	public BSTMap right;
-	public BSTMap parent;
-	private int size;
-	private K[] keySet;
-	
-	
-	
-	public BSTMap(K k, V v){
-		key=k;
-		val=v;
-		size=1;
-		left=null;
-		right=null;
-	}
 	
 	public BSTMap(){
-		key=null;
-		val=null;
-		size=0;
-		left=null;
-		right=null;
+		T=null;
 	}
 	
-	
-	//Return the BSTMap item whose key equals to sk, or null if sk not found.
-	public BSTMap find(BSTMap T, K sk){
-		if(T==null){
+
+	//Return the BST item whose key equals to sk, or null if sk not found.
+	public BST find(BST bst, K sk){
+		if(bst==null){
 			return null;
 		}
-		if(sk.equals(key))
+		if(sk.equals(bst.key()))
 			return T;
 		else {
-			int res=sk.compareTo(key);
+			int res=sk.compareTo((K) bst.key());
 			if (res<0){
-				return find(T.left, sk);
+				return find(bst.left, sk);
 			}else{
-				return find(T.right, sk);
+				return find(bst.right, sk);
 			}
 		}
 	}
 	
-	//Return the BSTMap that has inserted a size-1 BSTMap with key k and 
+
+	//Return the BST that has inserted a size-1 BSTMap with key k and 
 	//val v in the right place as a leaf.
-	public BSTMap insert(BSTMap T, K k, V v){
-		if(T==null){
-			return new BSTMap(k, v);
+	public BST insert(BST bst, K k, V v){
+		if(bst==null){
+			return new BST(k, v);
 		}else{
-			int res=k.compareTo(key);//Warning!!!!!-----------------------------------------
+			int res=k.compareTo((K) bst.key());//Warning!!!!!-----------------------------------------
 			if(res<0){
-				T.left=insert(T.left, k, v);
+				bst.left=insert(bst.left, k, v);
 			}else{
-				T.right=insert(T.right, k, v);
+				bst.right=insert(bst.right, k, v);
 			}
 		}	
-		T.size=T.left.size()+T.right.size()+1;
-		return T;
+		bst.size=1;
+		if (bst.left!=null){
+			bst.size+=bst.left.size();
+		}
+		if (bst.right!=null){
+			bst.size+=bst.right.size();
+		}
+		return bst;
 	}
 	
+
 	
-	private class Node{
-		K label;
-		V val;
-		Node left;
-		Node right;
+	private class BST<K extends Comparable<K>, V>{
+		private K key;
+		private V val;
+		public BST left;
+		public BST right;
+		public int size;
+		private K[] keySet;
 		
-		Node(K l, V v, Node Left, Node Right){
-			label=l;
+		public BST(K k, V v){
+			key=k;
 			val=v;
-			left=Left;
-			right=Right;
-		}
-		//return a Node that contains the label of l
-		Node get(K l){
-			if (l!=null&&l.equals(label)){
-				return this;
-			}
-			int res=l.compareTo(label);
-			if(res<0){
-				if(left==null){
-					return null;
-				}
-				return left.get(l);//Major Difference!!!!! UNKNOWN MEANING IN ULLMap.java!
-			}//                      ^
-			else{
-				if(right==null){
-					return null;
-				}
-				return right.get(l);
-			}
+			size=1;
+			left=null;
+			right=null;
 		}
 		
+		public BST(){
+			key=null;
+			val=null;
+			size=0;
+			left=null;
+			right=null;
+		}
 		
+		public int size() {
+			return size;
+		}
+
+		public void setKey(K k){
+				key=k;
+		}
+
+		public void setVal(V v){
+				val=v;
+		}
+			
+		public K key(){
+				return key;
+		}
+			
+		public V value(){
+				return val;
+		}
+				
 	};
+		
 	@Override
 	public Iterator<K> iterator() {
 		throw new UnsupportedOperationException();
@@ -111,19 +109,15 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 
 	@Override
 	public void clear() {
-		key=null;
-		val=null;
-		left=null;
-		right=null;
-		size=0;
+		T=null;
 	}
 	
 
 	
 	@Override
 	public boolean containsKey(K key) {
-		BSTMap T=find(this, key);
-		if(T==null){
+		BST bst=find(T, key);
+		if(bst==null){
 			return false;
 		}
 		return true;
@@ -131,25 +125,29 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 
 	@Override
 	public V get(K key) {
-		BSTMap T=find(this, key);
-		if(T==null){
+		BST bst=find(T, key);
+		if(bst==null){
 			return null;
 		}
-		return (V) T.value();
+		return (V) bst.value();
 	}
 
 	@Override
 	public int size() {
-		return size;
+		if(T==null){
+			return 0;
+		}
+		return T.size();
 	}
 
 	@Override
 	public void put(K key, V value) {
-		BSTMap T=find(this, key);
-		if(T==null){
-			insert(this, key, value);
+		BST bst=find(T, key);
+		if(bst==null){
+			T=insert(T, key, value);
+		}else{
+			T.setVal(value);
 		}
-		T.setVal(value);
 	}
 
 	@Override
@@ -181,20 +179,5 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
 		
 	}*/
 	
-	public void setKey(K k){
-		key=k;
-	}
-
-	public void setVal(V v){
-		val=v;
-	}
-	
-	public K key(){
-		return key;
-	}
-	
-	public V value(){
-		return val;
-	}
 	
 }
